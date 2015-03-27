20d2c2622a3b08cb4802bd563e0b248250fc58f1
jETA-Kor <jeta@jetalog.net>
2015-03-28 00:08:20 +0900

    추첨 기능 구현
    * 참여 인원을 입력하면 몇 번 째 선택에서 상품을 당첨시킬 것인지를 출력함
    * 디자인 적용 예정

--- js/common.js
+++ js/common.js
@@ -1,5 +1,51 @@
+// initialize
 $(document).ready(function() {
-    alert('I ready!');
-    
-    $('body').prepend('Ready!');
+//    $('#btn_start').click(generate($('#num_join').val()));
+    $('#btn_start').bind('click', function() {
+        generate($('#num_join').val());
+    });
 });
+
+function getRandom(number) {
+    return Math.floor(Math.random() * number);
+}
+
+// Generator
+function generate(numbers) {
+    var result = new Array();
+    
+    for(var i = 0; i < numbers; i++)
+        result[i] = 0;
+    
+    for(var i = 0; i < 9; i++) {
+        var j = getRandom(numbers);
+        
+        while(result[j] != 0) {
+            j = getRandom(numbers);
+        }
+        
+        switch(i) {
+            case 0:
+            case 1:
+            case 2:
+                result[j] = 1;
+                break;
+            case 3:
+            case 4:
+            case 5:
+                result[j] = 2;
+                break;
+            case 6:
+            case 7:
+            case 8:
+                result[j] = 3;
+                break;
+        }
+    }
+    
+    $('#result').html('');
+    
+    for(var i = 0; i < numbers; i++) {
+        $('#result').append(i + ': ' + result[i] + '<br />');
+    }
+}
