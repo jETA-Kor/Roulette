16f8c2011b84649595af8c56ae7ce6d67c3ff684
jETA-Kor <jeta@jetalog.net>
2015-03-29 12:09:58 +0900

    기본 구조 완성
    * 전체 사용 흐름에 따른 구조 완성
    * 디자인 적용 예정

--- js/common.js
+++ js/common.js
@@ -1,15 +1,31 @@
 // initialize
 $(document).ready(function() {
-    $('.page').hide();
+    $('.page').hide(); // 모든 페이지 숨기기
     
-    $('#btn_start').bind('click', function() {
+    $('#btn_start').click(function() {
         generate($('#num_join').val());
-        $('#pg_result').show();
-    });
+        $('#pg_initializer').hide();
+        $('#pg_shuffle').show();
+    }); // 시작 버튼 이벤트 바인딩
     
-    $('#btn_next').bind('click', function() {
+    $('#pg_shuffle input').click(function() {
+        $('#pg_shuffle').fadeOut(500);
+        $('#pg_cardSelector').fadeIn(500);
+    }); // 카드 선택 화면으로 이동 이벤트 바인딩
+    
+    $('#pg_cardSelector input').click(function() {
+        $('#pg_cardSelector').fadeOut(500, function() {
+            $('#pg_result').fadeIn(500);
+        });
+        
         showNext();
-    });
+    }); // 결과 출력 이벤트 바인딩
+    
+    $('#btn_next').click(function() {
+        $('#pg_result').fadeOut(500, function() {
+            $('#pg_cardSelector').fadeIn(500);
+        });
+    }); // 다음 결과 보기 이벤트 바인딩
     
     currentIndex = 0;
     result = null;
